c38cab294d90df1b3578c34ed91da5be4b70e07b
Felipe <felipe.b31@gmail.com>
2016-10-15 15:28:33 -0300

    Extraindo metodo para calculo de preco pela quantidade disponivel

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -10,22 +10,12 @@ public class CalculadoraDePrecos {
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
 		BigDecimal preco;
 		
-		
 		TipoDeEspetaculo tipo = sessao.getEspetaculo().getTipo();
 		
 		if(tipo.equals(TipoDeEspetaculo.CINEMA) || tipo.equals(TipoDeEspetaculo.SHOW)) {
-			//quando estiver acabando os ingressos... 
-			if(porcetagemDeLugaresDisponiveis(sessao) <= 0.05) { 
-				preco = reajustaPreco(sessao, 0.10);
-			} else {
-				preco = sessao.getPreco();
-			}
+			preco = calculaPrecoPorQuantidadeDisponivel(sessao, 0.05, 0.10);
 		} else if(tipo.equals(TipoDeEspetaculo.BALLET) || tipo.equals(TipoDeEspetaculo.ORQUESTRA)) {
-			if(porcetagemDeLugaresDisponiveis(sessao) <= 0.50) { 
-				preco = reajustaPreco(sessao,0.20);
-			} else {
-				preco = sessao.getPreco();
-			}
+			preco = calculaPrecoPorQuantidadeDisponivel(sessao, 0.50, 0.20);
 			
 			if(sessao.getDuracaoEmMinutos() > 60){
 				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
@@ -38,12 +28,25 @@ public class CalculadoraDePrecos {
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
 
+	private static BigDecimal calculaPrecoPorQuantidadeDisponivel(
+			Sessao sessao, double ocupacao, double ajuste) {
+		BigDecimal preco;
+		if (porcetagemDeLugaresDisponiveis(sessao) <= ocupacao) {
+			preco = reajustaPreco(sessao, ajuste);
+		} else {
+			preco = sessao.getPreco();
+		}
+		return preco;
+	}
+
 	private static BigDecimal reajustaPreco(Sessao sessao, double reajuste) {
-		return sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(reajuste)));
+		return sessao.getPreco().add(
+				sessao.getPreco().multiply(BigDecimal.valueOf(reajuste)));
 	}
 
 	private static double porcetagemDeLugaresDisponiveis(Sessao sessao) {
-		return (sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue();
+		return (sessao.getTotalIngressos() - sessao.getIngressosReservados())
+				/ sessao.getTotalIngressos().doubleValue();
 	}
 
 }
