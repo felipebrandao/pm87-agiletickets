50c75aa5393e2d82384195caae8da7a5da9b751f
Felipe <felipe.b31@gmail.com>
2016-10-15 15:33:46 -0300

    Extraindo metodo para incrementacao do preco

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -8,33 +8,27 @@ import br.com.caelum.agiletickets.models.TipoDeEspetaculo;
 public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
-		BigDecimal preco;
+		BigDecimal preco = sessao.getPreco();
 		
 		TipoDeEspetaculo tipo = sessao.getEspetaculo().getTipo();
 		
 		if(tipo.equals(TipoDeEspetaculo.CINEMA) || tipo.equals(TipoDeEspetaculo.SHOW)) {
-			preco = calculaPrecoPorQuantidadeDisponivel(sessao, 0.05, 0.10);
+			preco = ajustaPrecoPorQuantidadeDisponivel(preco, sessao, 0.05, 0.10);
 		} else if(tipo.equals(TipoDeEspetaculo.BALLET) || tipo.equals(TipoDeEspetaculo.ORQUESTRA)) {
-			preco = calculaPrecoPorQuantidadeDisponivel(sessao, 0.50, 0.20);
+			preco = ajustaPrecoPorQuantidadeDisponivel(preco, sessao, 0.50, 0.20);
 			
 			if(sessao.getDuracaoEmMinutos() > 60){
 				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			}
-		}  else {
-			//nao aplica aumento para teatro (quem vai é pobretão)
-			preco = sessao.getPreco();
 		} 
 
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
 
-	private static BigDecimal calculaPrecoPorQuantidadeDisponivel(
-			Sessao sessao, double ocupacao, double ajuste) {
-		BigDecimal preco;
+	private static BigDecimal ajustaPrecoPorQuantidadeDisponivel(
+			BigDecimal preco, Sessao sessao, double ocupacao, double ajuste) {
 		if (porcetagemDeLugaresDisponiveis(sessao) <= ocupacao) {
 			preco = reajustaPreco(sessao, ajuste);
-		} else {
-			preco = sessao.getPreco();
 		}
 		return preco;
 	}
