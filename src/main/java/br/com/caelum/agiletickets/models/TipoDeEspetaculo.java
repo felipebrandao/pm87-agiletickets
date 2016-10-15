81c9867246d5047f53e71708d72101212b3ede4f
Felipe <felipe.b31@gmail.com>
2016-10-15 16:11:38 -0300

    Extraindo metodos de calculoDePreco da enum TipoEspetaculo

--- src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
@@ -7,16 +7,12 @@ public enum TipoDeEspetaculo {
 	CINEMA {
 		@Override
 		public BigDecimal calculaPreco(Sessao sessao) {
-			BigDecimal preco = sessao.getPreco();
-			preco = sessao.ajustaPrecoPorQuantidadeDisponivel(preco, 0.05, 0.10);
-			return preco;
+			return calculaPrecoDeEspetaculoSimples(sessao);
 		}
 	}, SHOW {
 		@Override
 		public BigDecimal calculaPreco(Sessao sessao) {
-			BigDecimal preco = sessao.getPreco();
-			preco = sessao.ajustaPrecoPorQuantidadeDisponivel(preco, 0.05, 0.10);
-			return preco;
+			return calculaPrecoDeEspetaculoSimples(sessao);
 		}
 	}, TEATRO {
 		@Override
@@ -26,20 +22,29 @@ public enum TipoDeEspetaculo {
 	}, BALLET {
 		@Override
 		public BigDecimal calculaPreco(Sessao sessao) {
-			BigDecimal preco = sessao.getPreco();
-			preco = sessao.ajustaPrecoPorQuantidadeDisponivel(preco, 0.50, 0.20);
-			preco = sessao.ajustaPrecoPorDuracao(preco);
-			return preco;
+			return calculaParaEspetaculoChique(sessao);
 		}
+
+		
 	}, ORQUESTRA {
 		@Override
 		public BigDecimal calculaPreco(Sessao sessao) {
-			BigDecimal preco = sessao.getPreco();
-			preco = sessao.ajustaPrecoPorQuantidadeDisponivel(preco, 0.50, 0.20);
-			preco = sessao.ajustaPrecoPorDuracao(preco);
-			return preco;
+			return calculaParaEspetaculoChique(sessao);
 		}
 	};
 	
 	public abstract BigDecimal calculaPreco(Sessao sessao);
+	
+	private static BigDecimal calculaParaEspetaculoChique(Sessao sessao) {
+		BigDecimal preco = sessao.getPreco();
+		preco = sessao.ajustaPrecoPorQuantidadeDisponivel(preco, 0.50, 0.20);
+		preco = sessao.ajustaPrecoPorDuracao(preco);
+		return preco;
+	}
+
+	private static BigDecimal calculaPrecoDeEspetaculoSimples(Sessao sessao) {
+		BigDecimal preco = sessao.getPreco();
+		preco = sessao.ajustaPrecoPorQuantidadeDisponivel(preco, 0.05, 0.10);
+		return preco;
+	}
 }
