5d8f65ae31f1ca0b0c9e7cdd4f15c150f8d3feec
Felipe <felipe.b31@gmail.com>
2016-10-15 16:07:14 -0300

    Movendo metodo calculaPreco para Enum TipoEspetaculo

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -3,23 +3,11 @@ package br.com.caelum.agiletickets.domain.precos;
 import java.math.BigDecimal;
 
 import br.com.caelum.agiletickets.models.Sessao;
-import br.com.caelum.agiletickets.models.TipoDeEspetaculo;
 
 public class CalculadoraDePrecos {
 
-	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
-		BigDecimal preco = sessao.getPreco();
-		
-		TipoDeEspetaculo tipo = sessao.getEspetaculo().getTipo();
-		
-		if(tipo.equals(TipoDeEspetaculo.CINEMA) || tipo.equals(TipoDeEspetaculo.SHOW)) {
-			preco = sessao.ajustaPrecoPorQuantidadeDisponivel(preco, 0.05, 0.10);
-		} else if(tipo.equals(TipoDeEspetaculo.BALLET) || tipo.equals(TipoDeEspetaculo.ORQUESTRA)) {
-			preco = sessao.ajustaPrecoPorQuantidadeDisponivel(preco, 0.50, 0.20);
-			
-			preco = sessao.ajustaPrecoPorDuracao(preco);
-		} 
-
+	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {		
+		BigDecimal preco = sessao.getEspetaculo().getTipo().calculaPreco(sessao);
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
 }
--- src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
@@ -1,7 +1,45 @@
 package br.com.caelum.agiletickets.models;
 
+import java.math.BigDecimal;
+
 public enum TipoDeEspetaculo {
 	
-	CINEMA, SHOW, TEATRO, BALLET, ORQUESTRA;
+	CINEMA {
+		@Override
+		public BigDecimal calculaPreco(Sessao sessao) {
+			BigDecimal preco = sessao.getPreco();
+			preco = sessao.ajustaPrecoPorQuantidadeDisponivel(preco, 0.05, 0.10);
+			return preco;
+		}
+	}, SHOW {
+		@Override
+		public BigDecimal calculaPreco(Sessao sessao) {
+			BigDecimal preco = sessao.getPreco();
+			preco = sessao.ajustaPrecoPorQuantidadeDisponivel(preco, 0.05, 0.10);
+			return preco;
+		}
+	}, TEATRO {
+		@Override
+		public BigDecimal calculaPreco(Sessao sessao) {
+			return sessao.getPreco();
+		}
+	}, BALLET {
+		@Override
+		public BigDecimal calculaPreco(Sessao sessao) {
+			BigDecimal preco = sessao.getPreco();
+			preco = sessao.ajustaPrecoPorQuantidadeDisponivel(preco, 0.50, 0.20);
+			preco = sessao.ajustaPrecoPorDuracao(preco);
+			return preco;
+		}
+	}, ORQUESTRA {
+		@Override
+		public BigDecimal calculaPreco(Sessao sessao) {
+			BigDecimal preco = sessao.getPreco();
+			preco = sessao.ajustaPrecoPorQuantidadeDisponivel(preco, 0.50, 0.20);
+			preco = sessao.ajustaPrecoPorDuracao(preco);
+			return preco;
+		}
+	};
 	
+	public abstract BigDecimal calculaPreco(Sessao sessao);
 }
