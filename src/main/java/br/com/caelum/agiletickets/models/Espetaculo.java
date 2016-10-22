9e43b2a3a19a37ae6dcce39b8924358c3d926759
Felipe <felipe.b31@gmail.com>
2016-10-22 12:06:10 -0200

    Criado método para criação de sessão

--- src/main/java/br/com/caelum/agiletickets/models/Espetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/Espetaculo.java
@@ -2,6 +2,7 @@ package br.com.caelum.agiletickets.models;
 
 import static com.google.common.collect.Lists.newArrayList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -98,7 +99,11 @@ public class Espetaculo {
      */
 	public List<Sessao> criaSessoes(LocalDate inicio, LocalDate fim, LocalTime horario, Periodicidade periodicidade) {
 		// ALUNO: Não apague esse metodo. Esse sim será usado no futuro! ;)
-		return null;
+		Sessao sessao = new Sessao();
+		List<Sessao> listaSessao = new ArrayList<Sessao>();
+		listaSessao.add(sessao);
+		
+		return listaSessao;
 	}
 	
 	public boolean Vagas(int qtd, int min)
